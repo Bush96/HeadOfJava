01aa3bad315abdc9307363d844d2488032e5b063
Bush96 <92211699+Bush96@users.noreply.github.com>
2022-04-26 15:01:20 +0300

    Add files via upload

---
+++ HeadOfJava1st/src/exersize1HOJ.java
@@ -0,0 +1,112 @@
+public class exersize1HOJ {
+    /*public static void main(String[] args) {
+        int x = 1;
+        while (x<3){
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x= x+1;
+    }
+        if (x==3){
+            System.out.print("Do");
+}}
+}
+     */
+    /*public static void main(String[] args) {
+        int beerNum = 99;
+        String word = "бутылок (бутылки)";
+        while (beerNum > 0) {
+            if (beerNum == 1) {
+                word = "бутылка";
+            }
+            System.out.println((beerNum + " " + word + " Пива на стене"));
+            System.out.println(beerNum + " " + word + " пива");
+            System.out.println("Возьми одну.");
+            System.out.println("Пусти по округу.");
+            beerNum = beerNum - 1;
+            if (beerNum == 0) {
+                System.out.println("Нет бутылок пива на стене");
+
+            }
+        }
+    }
+
+     */
+    /*public static void main(String[] args) {
+        String[] pets = {"Фидо", "Басс", "Гордон"};
+        String[] going = {"Бегать", "Прыгать", "Гулять"};
+        int x = pets.length;
+        int y = going.length;
+        int rand1 = (int) (Math.random() * x);
+        int rand2 = (int) (Math.random() * y);
+        String dogs = pets[rand1];
+        String got = going[rand2];
+        System.out.println(dogs + " " + got);
+
+    }
+
+     */
+    /*public static void main(String[] args) {
+        int x = 3;
+        while (x > 0) {
+            if (x > 2) {
+                System.out.print("a");
+            }
+            x = x - 1;
+            System.out.print("-");
+            if (x == 2) {
+                System.out.print("b c");
+            }
+            if (x ==1 ){
+                System.out.print("d");
+                x = x-1;
+            }
+        }
+    }
+
+     */
+/*
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 0;
+        while (x < 5) {
+            if (y < 5) {
+                x = x + 1;
+                if (y < 3) {
+                    x = x - 1;
+                }
+            }
+            y = y + 2;
+            System.out.print(x + "" + y + " ");
+            x = x + 1;
+        }
+    }
+
+ */
+/*
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 4) {
+            System.out.print("a");
+            if (x < 1) {
+                System.out.print(" ");
+            }
+            System.out.println("n");
+            if (x > 1) {
+                System.out.print(" oyster");
+                x = x + 2;
+            }
+            if (x == 1) {
+                System.out.print("noys");
+            }
+            if (x < 1) {
+                System.out.print("oise");
+            }
+            System.out.println("");
+            x = x + 1;
+        }
+    }
+
+ */
+}
+
+
